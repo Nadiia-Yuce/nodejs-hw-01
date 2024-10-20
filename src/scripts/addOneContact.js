ed8681aaa2b4c5e5cad853d42885a6672e0fa60f
Nadiia Yuce <soyka9616@gmail.com>
2024-10-20 13:31:49 +0200

    all tasks

---
+++ src/scripts/addOneContact.js
@@ -0,0 +1,13 @@
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const contacts = await readContacts();
+  const newContact = createFakeContact();
+  contacts.push(newContact);
+  await writeContacts(contacts);
+  //   await writeContacts([...contacts, newContact]);
+};
+
+addOneContact();
